e711dd8e01a907e1c5cb8b8c204b1616ae6b7964
Matthias Alt <mattagohni@gmail.com>
2025-11-01 12:48:56 +0100

    feat: configure nordvim and neotree plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -38,6 +38,41 @@ vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts)
 vim.keymap.set('n', '<leader>x', ':bdelete!<CR>', opts)
 vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', opts)
 
+-- Window management
+vim.keymap.set('n', '<leader>v', '<C-w>v', opts)
+vim.keymap.set('n', '<leader>h', '<C-w>s', opts)
+vim.keymap.set('n', '<leader>se', '<C-w>=', opts)
+vim.keymap.set('n', '<leader>xs', ':close<CR>', opts)
+
+-- Navigate between splits
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', opts)
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', opts)
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', opts)
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', opts)
+
+-- Tabs
+vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts)
+vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts)
+vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts)
+vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts)
+
+-- Toggle line wrapping
+vim.keymap.set('n', '<leader>lw', '<cmd>set wrap!<CR>', opts)
+
+
+-- Stay in indent mode
+vim.keymap.set('v', '<', '<gv', opts)
+vim.keymap.set('v', '>', '>gv', opts)
+
+-- keep last yanked when pasting
+vim.keymap.set('v', 'p', '"_dP', opts)
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to last diagnostic' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic' })
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open all diagnostics in a flaoting window' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = '' })
+
 
 -- Custom Mattagohni !!!
 -- open ProjectFiles with Neotree
